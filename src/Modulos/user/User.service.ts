156c0c0fd68c5cf7fa7530d65b7a6f4156764f00
Ed_sh <edsghot@gmail.com>
2023-11-28 01:34:29 -0500

    fix:

--- src/Modulos/user/User.service.ts
+++ src/Modulos/user/User.service.ts
@@ -23,10 +23,7 @@ export class UserService {
       }
       async createUser(user: createUserHuevo) {
         const { Nombre,Email, Password } = user;
-        
-        const existingUser = await this.userRepository.findOne({
-          where: { Email,Nombre }
-        });
+
         const newUser = new User();
         newUser.Nombre = Nombre;
         newUser.Email = Email;
